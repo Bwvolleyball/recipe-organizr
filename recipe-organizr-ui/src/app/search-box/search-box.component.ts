e7cc2dc1eaead81c3c317c13c6d71a6ae8d09caa
Brandon Ward <ward486@regis.edu>
2019-05-27 21:39:05 -0600

    Bug fixes (#12)
    * Fixing search box not resetting, footer with no padding, adding profile button

--- recipe-organizr-ui/src/app/search-box/search-box.component.ts
+++ recipe-organizr-ui/src/app/search-box/search-box.component.ts
@@ -4,6 +4,7 @@ import {RecipeSearchService} from '../../../projects/recipe/src/app/recipe-searc
 import {Router} from '@angular/router';
 import {Subscription} from 'rxjs';
 
+
 @Component({
   selector: 'app-search-box',
   templateUrl: './search-box.component.html',
@@ -18,7 +19,8 @@ export class SearchBoxComponent implements OnInit, OnDestroy {
 
   predictionSubscription: Subscription;
 
-  constructor(private recipeSearchService: RecipeSearchService, private router: Router) {
+  constructor(private recipeSearchService: RecipeSearchService,
+              private router: Router) {
   }
 
   ngOnInit() {
@@ -29,15 +31,23 @@ export class SearchBoxComponent implements OnInit, OnDestroy {
     this.predictionSubscription.unsubscribe();
   }
 
-  searchUpdated() {
-    this.recipeSearchService.predict(this.searchName);
+  searchUpdated(override?: string) {
+    const searchTerm = override === undefined ? this.searchName : override;
+    this.recipeSearchService.predict(searchTerm);
   }
 
   searchRecipes() {
-    this.router.navigateByUrl('/recipe/list/' + this.searchName).then();
+    this.router.navigateByUrl('/recipe/list/' + this.searchName).then(() => this.resetSearchResults());
   }
 
   selectRecipe(name: string) {
-    this.recipeSearchService.search(name).subscribe(recipe => this.router.navigateByUrl('/recipe/detail/' + recipe[0].id));
+    this.recipeSearchService.search(name)
+      .subscribe(recipe => this.router.navigateByUrl('/recipe/detail/' + recipe[0].id)
+        .then(this.resetSearchResults));
+  }
+
+  private resetSearchResults() {
+    // We need to wipe out the observable's pre-fill list.
+    this.searchUpdated('');
   }
 }
